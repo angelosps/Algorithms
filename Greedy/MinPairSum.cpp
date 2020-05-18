005785e836a12a8f3aba46595020a9b2cbf0568f
Angelos Poulis <64834761+angelosps@users.noreply.github.com>
2020-05-18 12:41:06 +0300

    Add files via upload

---
+++ Greedy/MinPairSum.cpp
@@ -0,0 +1,24 @@
+// Forming pairs such that the maximum pair sum is minimized.
+// Greedy approach 
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(void){
+int N; cin>>N;
+
+int arr[N];
+
+for(int i = 0 ; i < N ; i++) cin >> arr[i];
+
+sort(arr, arr+N);
+
+for(int i = 0, j = N-1 ; i<=j ; i++, j--)
+    cout << "(" << arr[i] << ", " << arr[j] << ") ";
+
+cout << endl;
+
+    return 0;
+}
