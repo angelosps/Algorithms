66995e17d62d8f8de09f2c76d980d214992db81e
Angelos Poulis <64834761+angelosps@users.noreply.github.com>
2021-03-10 15:21:46 +0200

    Update SegmentTreeClassic.cpp

--- Data Structures/SegmentTreeClassic.cpp
+++ Data Structures/SegmentTreeClassic.cpp
@@ -8,50 +8,53 @@ using namespace std;
 
 int arr[100000], seg[400000];
 
-void build(int s, int e, int idx){
-    if(s==e){
-        seg[idx]=arr[s];
+void build(int s, int e, int idx) {
+    if(s == e){
+        seg[idx] = arr[s];
         return;
     }
 
-    int mid=(s+e)/2;
+    int mid = (s+e)/2;
     build(s, mid, idx*2);
     build(mid+1, e, idx*2+1);
-    seg[idx]=min(seg[idx*2], seg[idx*2+1]);
+    seg[idx] = min(seg[idx*2], seg[idx*2+1]);
 }
 
-void update(int s, int e, int us, int ue, int k, int idx){
-    if(s>=us && e<=ue){
-        seg[idx]+=k;
-        arr[s]+=k;
+void update(int s, int e, int us, int ue, int k, int idx) {
+    if(s >= us && e <= ue){
+        seg[idx] += k;
+        arr[s] += k;
         return;
     }
-    else if( e<us || s>ue ) return;
+    else if(e < us || s > ue) return;
 
     int mid = (s+e)/2;
 
     update(s, mid, us, ue, k, idx*2);
     update(mid+1, e, us, ue, k, idx*2+1);
-    seg[idx]=min(seg[idx*2], seg[idx*2+1]);
+    seg[idx] = min(seg[idx*2], seg[idx*2+1]);
 }
 
 
-int query(int s, int e, int qs, int qe, int idx){
-    if(s>=qs && e<=qe)
+int query(int s, int e, int qs, int qe, int idx) {
+    if(s >= qs && e <= qe)
         return seg[idx];
-    else if(s>qe || e<qs) return INF;
+    else if(s > qe || e < qs) 
+        return INF;
 
-    int mid=(s+e)/2;
+    int mid = (s+e)/2;
 
-    int p1=query(s, mid, qs, qe, idx*2);
-    int p2=query(mid+1, e, qs, qe, idx*2+1);
+    int p1 = query(s, mid, qs, qe, idx*2);
+    int p2 = query(mid+1, e, qs, qe, idx*2+1);
     return min(p1, p2);
 }
 
 int main(void) {
-    int n; cin>>n;
+    int n; 
+    scanf("%d", &n);
 
-    for(int i=0; i<n; i++) cin>>arr[i];
+    for(int i = 0; i < n; i++) 
+        scanf("%d", &arr[i]);
 
     build(0, n-1, 1);
 
