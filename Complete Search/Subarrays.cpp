dfd322b39a3456970ffcd6faa81998e5fa6e4d6f
angelosps <64834761+angelosps@users.noreply.github.com>
2021-03-16 18:20:36 +0200

    Update Subarrays.cpp
    2D subarrays print bug fixed

--- Complete Search/Subarrays.cpp
+++ Complete Search/Subarrays.cpp
@@ -8,42 +8,57 @@
 using namespace std;
 
 int main() {
-    int a[N+1];
+    int a[N+1], cnt1 = 0, cnt2 = 0;
 
-    for (int i = 0; i < N; i++)
+    cout << "Generating the subarrays for the 1D array below . . .\n";
+
+    for (int i = 0; i < N; i++) {
         a[i] = i+1;
-
-    cout << "\nGenerating all the subarrays of the 1D array given . . .\n\n";
+        cout << a[i] << " ";
+    }
+    
+    cout << endl << endl;
 
     for (int i = 0; i < N; i++) {
         for (int j = i; j < N; j++) {
             for (int k = i; k <= j; k++)
                 cout << a[k] << " ";
-            cout << endl;
+            cout << "\n**********\n";
+            cnt1++;
         }
-        cout << "\n**********\n";
     }
+
+    cout << "\nNumber of 1D subarrays: " << cnt1 << endl << endl;
+
+    cout << "\nGenerating the subarrays for the 2D array below . . .\n";
 
     int arr[N+1][M+1];
     
-    for(int i = 0; i < N; i++)
-        for(int j = 0; j < M; j++)
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < M; j++) {
             arr[i][j] = (i*M)+j+1;
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
     
     cout << "\nGenerating all the subarrays of the 2D array given . . .\n\n";
 
-    for(int i = 0; i < N; i++)
-        for(int j = 0; j < M; j++)
-            for(int x = i; x < N; x++)
-                for(int y = j; y < M; y++){
-                    for(int a = i; a <= x; a++){
-                        for(int b = j; b <= y; b++){
+    for(int i=0; i<N; i++)
+        for(int j=0; j<M; j++)
+            for(int x=i; x<N; x++)
+                for(int y=j; y<M; y++) {
+                    for(int a=i; a<=x; a++) {
+                        for(int b=j; b<=y; b++) {
                             cout << arr[a][b] << " ";
                         }
                         cout << endl;
-                    }
-                    cout << "\n**********\n";
+                    }  
+                    cnt2++;
+                    cout << "*************" << endl;
                 }
+  
+    cout << "Number of 2D subarrays: " << cnt2 << endl << endl;
 
     return 0;
 }
