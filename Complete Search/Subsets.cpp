4e9de5ef00854bdad922c9ccc314d19c5c6a3f93
angelosps <64834761+angelosps@users.noreply.github.com>
2021-04-22 21:34:38 +0300

    added iteration

--- Complete Search/Subsets.cpp
+++ Complete Search/Subsets.cpp
@@ -1,4 +1,4 @@
-// Generating subsets
+// Generating subsets with both recursion and iteration
 
 #include <iostream>
 #include <vector>
@@ -7,23 +7,39 @@ using namespace std;
 vector<int> subset;
 int N;
 
-void process(int k) {
+void sub_iter() {
+    for (int i = 0; i < (1 << N); i++) {
+        printf("Subset: ");
+        if (i == 0) {
+            putchar(0xC3), putchar(0x98), putchar(10);
+            continue;
+        }
+        for (int j = 0; j < N; j++)
+            if (i & (1 << j))
+                printf("%d ", j);
+        printf("\n");
+    }
+}
+
+void sub_rec(int k) {
     if(k == N) {
-        cout << "Subset: ";
+        printf("Subset: ");
         if(subset.empty()) putchar(0xC3), putchar(0x98);
-        else for (auto x: subset) cout << x << " ";
+        else for (auto x: subset) printf("%d ", x);
         cout << endl;
     } else {
-        process(k+1);
+        sub_rec(k+1);
         subset.push_back(k);
-        process(k+1);
+        sub_rec(k+1);
         subset.pop_back();
     }
 }
 
 int main() {
     cin >> N;
-    process(0);
-    
+    printf("\nSubsets with recursion..\n");
+    sub_rec(0);
+    printf("\nSubsets with iteration..\n");
+    sub_iter();
     return 0;
 }
