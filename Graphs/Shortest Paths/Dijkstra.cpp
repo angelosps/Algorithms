e395a5622690bf4ccc51c121289fc157794f3d42
Angelos Poulis <64834761+angelosps@users.noreply.github.com>
2021-06-09 14:04:34 +0300

    tabs

--- Graphs/Shortest Paths/Dijkstra.cpp
+++ Graphs/Shortest Paths/Dijkstra.cpp
@@ -14,43 +14,44 @@ priority_queue< ii, vii, greater<ii> > pq;
 vi dist;
 
 void dijkstra(int s){
-dist[s]=0;
-pq.push(ii(0, s));
+    dist[s]=0;
+    pq.push(ii(0, s));
 
-while (!pq.empty()){
-    ii front = pq.top();
-    pq.pop();
+    while (!pq.empty()){
+        ii front = pq.top();
+        pq.pop();
 
-    int w = front.first;
-    int u = front.second;
+        int w = front.first;
+        int u = front.second;
 
-    if(w > dist[u]) continue;
+        if(w > dist[u]) continue;
 
-    for(int j=0; j<adjlist[u].size(); j++){
-        ii v=adjlist[u][j];
-        if (dist[u] + v.second < dist[v.first]){
-            dist[v.first] = dist[u]+v.second;
-            pq.push(ii(dist[v.first], v.first));
+        for(int j=0; j<adjlist[u].size(); j++){
+            ii v=adjlist[u][j];
+            if (dist[u] + v.second < dist[v.first]){
+                dist[v.first] = dist[u]+v.second;
+                pq.push(ii(dist[v.first], v.first));
+            }
         }
     }
 }
-}
 
 int main(void){
-int n, m, s;
-cin >> n >> m >> s;
-adjlist.assign(n, vii());
-dist.assign(n, inf);
+    int n, m, s;
+    cin >> n >> m >> s;
+    adjlist.assign(n, vii());
+    dist.assign(n, inf);
 
-for (int i=0; i<m; i++) {
-int a, b, w;
-    cin >> a >> b >> w;
-    adjlist[a].push_back(ii(b, w));
-}
+    for (int i=0; i<m; i++) {
+    int a, b, w;
+        cin >> a >> b >> w;
+        adjlist[a].push_back(ii(b, w));
+    }
 
-dijkstra(s);
+    dijkstra(s);
 
-for (int i=0; i<n; i++) cout << "Shortest Path ("<< s<<"," << i <<")=" << dist[i] << endl;
+    for (int i=0; i<n; i++) 
+        cout << "Shortest Path ("<< s<<"," << i <<") = " << dist[i] << endl;
 
-return 0;
+    return 0;
 }
