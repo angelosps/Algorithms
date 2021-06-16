c7aee50c8f216ea2da235d9608c6217d3bd3cd6c
Angelos Poulis <64834761+angelosps@users.noreply.github.com>
2021-06-16 21:24:22 +0300

    spaces fixed

--- Mathematics/PrimeFactorization.cpp
+++ Mathematics/PrimeFactorization.cpp
@@ -5,26 +5,24 @@
 using namespace std;
 vector<int> factors;
 
-void primefact(int x){
+void primefact(int x) {
+    for (int i = 2; i*i <= x; i++)
+        while (x % i == 0) {
+            factors.push_back(i), x/=i;
+        }
 
-for( int i = 2; i*i <= x; i++ )
-    while( x % i == 0 ){
-        factors.push_back(i), x/=i;
-    }
-
-if( x > 1 ) factors.push_back(x);
-
+    if (x > 1) factors.push_back(x);
 }
 
 int main(){
-int x; cin>>x;
+    int x; cin >> x;
 
-primefact(x);
+    primefact(x);
 
-cout << "Prime factorization of " << x << " is:\n";
-for(auto y : factors) cout << y << " ";
+    cout << "Prime factorization of " << x << " is:\n";
+    for (auto y : factors) cout << y << " ";
 
-cout << endl;
+    cout << endl;
 
     return 0;
 }
