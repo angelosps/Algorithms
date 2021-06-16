704a9528967a4b73cc224beb71634b2aa6944fb4
Angelos Poulis <64834761+angelosps@users.noreply.github.com>
2021-06-16 21:25:55 +0300

    spaces fixed

--- Mathematics/SieveofEratosthenes.cpp
+++ Mathematics/SieveofEratosthenes.cpp
@@ -5,28 +5,29 @@ using namespace std;
 
 bool sieve[N+1];
 
-void process( ){
+void process(){
 
-for(int i=2; i<=N; i++) (i%2==0 && i!=2) ? sieve[i]=0 : sieve[i]=1;
+    for(int i=2; i<=N; i++) 
+        (i%2==0 && i!=2) ? sieve[i]=0 : sieve[i]=1;
 
-for(int x=2; x*x<=N; x++){
-    if(!sieve[x]) continue;
-    for(int i=x*x; i<=N; i+=x) 
-        sieve[i]=0;
+    for(int x=2; x*x<=N; x++) {
+        if(!sieve[x]) continue;
+        for(int i=x*x; i<=N; i+=x) 
+            sieve[i] = 0;
+    }
+
 }
 
-}
+int main(void) {
+    process();
 
-int main(void){
+    cout << "The prime numbers in range[1, " << N << "] are:\n";
+    
+    for(int i=1; i<=N; i++) 
+        if(sieve[i]) 
+            cout << i << " ";
 
-process();
-
-cout<<"The prime numbers in range[1, "<<N<<"] are:\n";
-for(int i=1; i<=N; i++){
-    if(sieve[i]) cout<<i<<" ";
-}
-
-cout<<endl;
+    cout<<endl;
 
     return 0;
 }
